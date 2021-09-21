39f5aaa898333b7d5a09a34084baae1cd48a797a
Andres Haro <hvaandres@icloud.com>
2021-09-20 21:05:39 -0600

    Initial Commit

---
+++ Shared/PlanetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanetsApp.swift
+//  Shared
+//
+//  Created by Andres Haro on 9/20/21.
+//
+
+import SwiftUI
+
+@main
+struct PlanetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
